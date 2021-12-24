52ccb341beaf8e1dce4679d1a408c258d0732b02
Tequila1Boy <pushkin.rodion@yandex.ru>
2021-12-24 14:47:29 +0300

    da

--- js/script.js
+++ js/script.js
@@ -40,3 +40,25 @@ let slider = ()=>{
   });
 }
 timerFunction()
+$('.slider').slick({
+  arrows:true,
+  dots:true,
+  slidesToShow:4,
+  autoplay:true,
+  speed:1000,
+  autoplaySpeed:800,
+  // responsive:[
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       slidesToShow:2
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 550,
+  //     settings: {
+  //       slidesToShow:1
+  //     }
+  //   }
+  // ]
+});
